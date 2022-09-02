9f4976bbd80bb2e595b2826c2b46e40447c2f551
Vlatko Kosturjak <kost@linux.hr>
2022-09-02 08:47:30 +0200

    bugfixes

--- cmd/met/met.go
+++ cmd/met/met.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"github.com/kost/gosc/msf"
-	"github.com/spf13/pflag"
+	flag "github.com/spf13/pflag"
 )
 
 var remote string
@@ -11,11 +11,10 @@ var method string = "https"
 func ParseCommandLine() {
 	flag.StringVarP(&remote, "remote", "r", "127.0.0.1:4444", "host and port to connect to")
 	flag.StringVarP(&method, "method", "m", "https", "type of msf (http, https, tcp)")
-	flag.StringVarP(&execute, "execute", "e", "", "Specify string argument as SC to execute")
-	Parse()
+	flag.Parse()
 }
 
 func main() {
 	ParseCommandLine()
-	msf.meterpreter(method, remot)
+	msf.Meterpreter(method, remote)
 }
