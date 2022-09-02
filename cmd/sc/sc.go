183ef30343e1d5b89f02e8c7ad91b782f82fa958
Vlatko Kosturjak <kost@linux.hr>
2022-09-02 08:55:02 +0200

    add missing imports

--- cmd/sc/sc.go
+++ cmd/sc/sc.go
@@ -1,11 +1,12 @@
 package main
 
 import (
+	"os"
 	"github.com/kost/gosc/shell"
 )
 
 func main() {
 	for _, arg := range os.Args[1:] {
-		ExecShellCode_b64(arg)
+		shell.ExecShellCode_b64(arg)
 	}
 }
